1f7c4a55a08e5b5252965ce4b78de08100ff0e6c
Mark Swedberg <Mark Swedberg>
2017-02-15 20:20:54 -0800

    Server setup

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var middleware = require('./middleware.js');
+
+var app = express();
+var PORT = '3000';
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res) {
+    res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function() {
+    console.log('Express server running on http://localhost:' + PORT);
+});
